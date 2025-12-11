15b8befc21f0a1ad83f3c870535a9bdc52ae46bf
ReubenKimClemens <146577227+ReubenKimClemens@users.noreply.github.com>
2025-12-11 04:59:21 -0500

    api route

---
+++ src/routes/api/products/+server.js
@@ -0,0 +1,14 @@
+import { supabase } from '$lib/supabaseClient';
+import { json } from '@sveltejs/kit';
+
+export async function GET() {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*');
+
+  if (error) {
+    return json({ error: error.message }, { status: 500 });
+  }
+
+  return json(data, { status: 200 });
+}
